ff07a9f44dc553b2d268067ecf6bf31873c3d2ac
= <mmohmo@yahoo.com>
2024-10-07 14:15:21 -0400

    fig-accentblue-light class added

--- src/pages/ContactSearch.js
+++ src/pages/ContactSearch.js
@@ -18,36 +18,46 @@ export const ContactSearch = () => {
     }
 
     return (
-    <div className='fig-grad'>
-        <h3 className="text-2xl font-bold text-center mt-8">Search Contact</h3>
-        <form>
-            <div className="mt-8">
-                <input 
-                    onChange={handleSearchChange}
-                    className="bg-gray-100 py-4 w-full rounded-sm border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-300 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-                    type="text"
-                    name={searchBarVal}
-                    placeholder="search contact"
-                />
+    <div className='fig-grad p-8'>
+        <div className="bg-white p-8 rounded-md shadow-md">
+
+            <h3 className="text-2xl font-bold text-center mt-8">Search Contact</h3>
+            <form>
+                <div className="mt-8">
+                    <input 
+                        onChange={handleSearchChange}
+                        className="bg-gray-100 py-4 w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-300 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                        type="text"
+                        name={searchBarVal}
+                        placeholder="search contact"
+                    />
+                </div>
+                
+            </form>
+            <div className="divide-y divide-gray-200">
+                {
+                    searchContact.length? searchContact.map(contact => {
+                        const { id } = contact
+                        return (    
+                            <ul>
+                                <div className="flex justify-between items-center">
+                                    <li className="text-blue-700">
+                                        <Link to={`/contacts/${id}`}>{contact.name}</Link>
+                                    </li>
+                                    <Link to={`/contacts/${id}`}>
+                                        <h3 className="fig-accentBlue-light fig-blue-txt p-3 font-bold my-2 rounded-full ">GR</h3>
+                                    </Link>
+                                </div>
+                                
+                            </ul>)
+                        }
+                    ) : <h3>No contact found!</h3>
+                }
             </div>
-            
-        </form>
-        <div>
-            {
-                searchContact.length? searchContact.map(contact => {
-                    const { id } = contact
-                    return (    <ul>
-                            <li className="text-blue-700">
-                                <Link to={`/contacts/${id}`}>{contact.name}</Link>
-                            </li>
-                        </ul>)
-                    }
-                ) : <h3>No contact found!</h3>
-            }
-        </div>
-        <div  className="mt-8 flex justify-between">
-            <Link className="bg-blue-700 hover:bg-blue-500 text-white py-2 rounded-md px-8" to="/contacts/create">Add Contact</Link>
-            <Link className="bg-blue-700 hover:bg-blue-500 text-white py-2 rounded-md px-8" to="/">Cancel</Link>
+            <div  className="mt-8 flex justify-between">
+                <Link className="fig-blue hover:bg-blue-500 text-white py-2 rounded-md px-8" to="/contacts/create">Add Contact</Link>
+                <Link className="fig-blue hover:bg-blue-500 text-white py-2 rounded-md px-8" to="/">Cancel</Link>
+            </div>
         </div>
     </div>
   )
--- src/pages/EditContactPage.js
+++ src/pages/EditContactPage.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 
 export const EditContactPage = () => {
   return (
-    <div>
-        <EditForm/>
+    <div className="fig-grad">
+      <EditForm/>
     </div>
   )
 }

55a67b1bf075e83e2df9322e25edcdd5cca52dbd
= <mmohmo@yahoo.com>
2024-11-13 06:45:57 -0500

    app.js unused imports removed

--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
-// React Router
 import { BrowserRouter as Router } from "react-router-dom";
-
-// Redux
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { Navbar } from "./components/Navbar";
-import { Panel } from "./components/Panel";
-import { HeroSection } from "./components/HeroSection";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -19,9 +12,7 @@ root.render(
     <React.StrictMode>
       <Provider store={store}>
         <Router>
-          {/* <Navbar/> */}
           <App /> 
-          {/*<HeroSection/>*/}
         </Router>
       </Provider>
     </React.StrictMode>
